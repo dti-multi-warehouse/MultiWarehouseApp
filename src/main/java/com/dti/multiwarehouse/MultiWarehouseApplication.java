042ae1ec8eda945460115622d546c1bd8d7a055b
Gerry Sheva <gerry.sheva@proton.me>
2024-08-16 19:29:28 +0700

    init

---
+++ src/main/java/com/dti/multiwarehouse/MultiWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.dti.multiwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiWarehouseApplication.class, args);
+    }
+
+}
