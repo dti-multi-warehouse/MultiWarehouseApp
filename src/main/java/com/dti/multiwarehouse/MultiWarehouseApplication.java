bc6c4c490412d2d72b82d599b596cbf3ca5fa6d4
Gerry Sheva <gerry.sheva@proton.me>
2024-08-26 22:15:26 +0700

    config: configure cloudinary

--- src/main/java/com/dti/multiwarehouse/MultiWarehouseApplication.java
+++ src/main/java/com/dti/multiwarehouse/MultiWarehouseApplication.java
@@ -1,8 +1,11 @@
 package com.dti.multiwarehouse;
 
+import com.dti.multiwarehouse.config.CloudinaryConfigProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+@EnableConfigurationProperties(CloudinaryConfigProperties.class)
 @SpringBootApplication
 public class MultiWarehouseApplication {
 
---
+++ src/main/java/com/dti/multiwarehouse/config/CloudinaryConfigProperties.java
@@ -0,0 +1,14 @@
+package com.dti.multiwarehouse.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@Data
+@ConfigurationProperties(prefix = "cloudinary")
+public class CloudinaryConfigProperties {
+    private String cloudName;
+    private String apiKey;
+    private String apiSecret;
+    private String url;
+}
+
