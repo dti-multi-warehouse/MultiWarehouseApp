428508a0cea8c258be9ff55b8db1e7c42cfbebea
Gerry Sheva <gerry.sheva@proton.me>
2024-08-26 22:36:37 +0700

    feat: implement upload image

---
+++ src/main/java/com/dti/multiwarehouse/cloudinary/service/impl/CloudinaryServiceImpl.java
@@ -0,0 +1,31 @@
+package com.dti.multiwarehouse.cloudinary.service.impl;
+
+import com.cloudinary.Cloudinary;
+import com.dti.multiwarehouse.cloudinary.service.CloudinaryService;
+import jakarta.annotation.Resource;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Service
+public class CloudinaryServiceImpl implements CloudinaryService {
+    @Resource
+    private Cloudinary cloudinary;
+    private final List<String> ALLOWED_FORMAT = List.of("jpg", "jpeg", "png");
+
+    @Override
+    public String uploadImage(MultipartFile file, String folderName) throws IOException {
+        HashMap<Object, Object> options = new HashMap<>();
+        options.put("folder", folderName);
+        options.put("allowed_format", ALLOWED_FORMAT);
+        Map uploadedFile = cloudinary.uploader().upload(file.getBytes(), options);
+        String publicId = (String) uploadedFile.get("public_id");
+        return cloudinary.url().secure(true).generate(publicId);
+    }
+}
