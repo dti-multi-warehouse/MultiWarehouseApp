561d87f6953993530e961270c503d1ed01a8163e
gerry-sheva <108265433+gerry-sheva@users.noreply.github.com>
2024-10-02 13:25:20 +0700

    feat: add fill field for chart's color

--- src/main/java/com/dti/multiwarehouse/dashboard/dto/response/GetProductCategorySalesReportResponseDto.java
+++ src/main/java/com/dti/multiwarehouse/dashboard/dto/response/GetProductCategorySalesReportResponseDto.java
@@ -12,11 +12,13 @@ import lombok.NoArgsConstructor;
 public class GetProductCategorySalesReportResponseDto {
     String name;
     int revenue;
+    String fill;
 
     public static GetProductCategorySalesReportResponseDto fromDto(RetrieveProductCategorySales dto) {
         return GetProductCategorySalesReportResponseDto.builder()
                 .name(dto.getName())
                 .revenue(dto.getRevenue())
+                .fill(String.format("var(--color-%s)", dto.getName()))
                 .build();
     }
 }
