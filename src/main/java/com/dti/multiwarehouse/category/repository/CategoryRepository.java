b9d70fe1e05256e4228a94e0141fcb3f086846c4
gerry-sheva <108265433+gerry-sheva@users.noreply.github.com>
2024-10-03 15:40:51 +0700

    fix: sort by id for getAllCategories

--- src/main/java/com/dti/multiwarehouse/category/repository/CategoryRepository.java
+++ src/main/java/com/dti/multiwarehouse/category/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.dti.multiwarehouse.category.repository;
 import com.dti.multiwarehouse.category.dao.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findAllByOrderByIdAsc();
 }
