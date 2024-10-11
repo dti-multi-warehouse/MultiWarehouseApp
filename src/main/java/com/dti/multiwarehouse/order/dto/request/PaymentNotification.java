2c5489e97e0b52b12e0b87e92a4072382159faff
gerry-sheva <108265433+gerry-sheva@users.noreply.github.com>
2024-10-11 07:38:05 +0700

    fix: add @JsonProperty for parsing

--- src/main/java/com/dti/multiwarehouse/order/dto/request/PaymentNotification.java
+++ src/main/java/com/dti/multiwarehouse/order/dto/request/PaymentNotification.java
@@ -1,5 +1,6 @@
 package com.dti.multiwarehouse.order.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,7 +9,10 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PaymentNotification {
+    @JsonProperty("transaction_status")
     private String transactionStatus;
+    @JsonProperty("transaction_id")
     private String transactionId;
+    @JsonProperty("fraud_status")
     private String fraudStatus;
 }
