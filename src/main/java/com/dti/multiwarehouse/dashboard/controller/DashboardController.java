5cf08422ce283ae2d3d5bbfb061c5cb6d13c0b50
gerry-sheva <108265433+gerry-sheva@users.noreply.github.com>
2024-10-12 10:15:25 +0700

    feat: implement authorization on controller level

--- src/main/java/com/dti/multiwarehouse/dashboard/controller/DashboardController.java
+++ src/main/java/com/dti/multiwarehouse/dashboard/controller/DashboardController.java
@@ -5,6 +5,9 @@ import com.dti.multiwarehouse.response.Response;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -19,10 +22,15 @@ public class DashboardController {
     private final DashboardService dashboardService;
 
     @GetMapping()
+    @PreAuthorize("hasAnyRole('admin', 'warehouse_admin')")
     public ResponseEntity<?> getMonthlyTotalSalesReport(
+            @AuthenticationPrincipal Jwt jwt,
             @RequestParam Long warehouseId,
             @RequestParam @DateTimeFormat(pattern="yyyy-MM-dd") LocalDate date
             ) {
+        if (jwt == null || jwt.getClaim("warehouse_id") == null || !jwt.getClaim("warehouse_id").equals(warehouseId)) {
+            return Response.failed("Invalid authority");
+        }
         var res =  dashboardService.getSalesReport(warehouseId, date);
         return Response.success("Successfully retrieved monthly total sales report", res);
     }
